1a9ce6a62d689110c39d160452ec74294f3c00ed
Bernadette Werneke <bernadettewerneke@gmail.com>
2023-05-22 12:08:38 +0200

    Initial Commit

---
+++ KontaktApp/KontaktAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KontaktAppApp.swift
+//  KontaktApp
+//
+//  Created by Bernadette Werneke on 22.05.23.
+//
+
+import SwiftUI
+
+@main
+struct KontaktAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
